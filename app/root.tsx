c1fe2a8aee012488e8461b0a3b7e9067c1e3dd49
amaotone <amane.suzu@gmail.com>
2024-05-16 21:32:21 +0900

    feat: add edit page

--- app/root.tsx
+++ app/root.tsx
@@ -11,12 +11,16 @@ import {
   useLoaderData,
 } from "@remix-run/react";
 import appStylesHref from "./app.css?url";
-
-import { getContacts } from "./data";
+import { createEmptyContact, getContacts } from "./data";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
 
 export const loader = async () => {
   const contacts = await getContacts();
